7adcbf9259b5e54e85bc8b311e1779835565f269
Jake Swanson <jakswa@gmail.com>
2021-05-07 21:15:50 -0400

    fuck, works, but yeesh

--- tests/trains_route.rs
+++ tests/trains_route.rs
@@ -1,17 +1,20 @@
-use actix_web::client::Client;
-use std::net::TcpListener;
+use actix_web::http::StatusCode;
+use actix_web::{test, App};
+use env_logger::Env;
+use ruxy::http_caches::HttpCaches;
+use ruxy::routes::get_trains;
 
 #[actix_rt::test]
 async fn trains_route_works() {
-    let base_uri = spawn_server();
-    let uri = format!("{}/trains", base_uri);
-    let resp = Client::default().get(uri).send().await;
-}
-
-fn spawn_server() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = ruxy::server::build_ssl(listener).expect("failed to bind");
-    tokio::spawn(server);
-    format!("https://127.0.0.1:{}", port)
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let mut app = test::init_service(
+        App::new()
+            .service(get_trains)
+            .wrap(actix_web::middleware::Logger::default())
+            .data(HttpCaches::default()),
+    )
+    .await;
+    let req = test::TestRequest::get().uri("/trains").to_request();
+    let resp = test::call_service(&mut app, req).await;
+    assert_eq!(resp.status(), StatusCode::OK);
 }
