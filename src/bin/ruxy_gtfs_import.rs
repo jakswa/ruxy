2db0429f4d14227982c5605483353ebbe18bb17c
Jake Swanson <jakswa@gmail.com>
2021-07-02 20:40:52 -0400

    more WIP: beta.8 bump + fast sqlite now

--- src/bin/ruxy_gtfs_import.rs
+++ src/bin/ruxy_gtfs_import.rs
@@ -3,54 +3,51 @@ use zip::read::ZipArchive; // needed for is_empty()
 
 #[derive(Debug, serde::Deserialize)]
 struct Trip {
-    trip_id: u64,
-    direction_id: u64,
-    block_id: u64,
-    shape_id: u64,
-    route_id: u64,
-    service_id: u64,
+    trip_id: i64,
+    direction_id: i64,
+    block_id: i64,
+    shape_id: i64,
+    route_id: i64,
+    service_id: i64,
 }
 
 #[actix_web::main]
 async fn main() {
     let settings = build_settings().unwrap();
-    let db_pool = migrated_pool(&settings).await.unwrap();
+    let mut conn = migrated_pool(&settings).await.unwrap()
+        .acquire().await.unwrap();
     let file = gtfs_zip_file(&settings.gtfs_zip_url).await.unwrap();
     let mut archive = ZipArchive::new(file).unwrap();
     let trips = archive.by_name(&"trips.txt").unwrap();
     let mut reader = csv::Reader::from_reader(trips);
+    sqlx::query("BEGIN").execute(&mut conn).await.unwrap();
     for result in reader.deserialize() {
         let trip: Trip = result.unwrap();
-        let mut query = sqlx::query("select 1 from trips where trip_id = ?")
-            .bind(trip.trip_id.to_string())
-            .fetch_optional(&db_pool)
-            .await
-            .unwrap();
-        if query.is_none() {
-            println!("inserting {}", trip.trip_id);
-            sqlx::query(
-                "INSERT INTO trips (trip_id, direction_id, block_id, shape_id, route_id, service_id)
-                 VALUES (?, ?, ?, ?, ?, ?)"
-                ).bind(trip.trip_id.to_string())
-                .bind(trip.direction_id.to_string())
-                .bind(trip.block_id.to_string())
-                .bind(trip.shape_id.to_string())
-                .bind(trip.route_id.to_string())
-                .bind(trip.service_id.to_string())
-                .execute(&db_pool).await.unwrap();
-        }
-        break;
+        insert_trip(&mut conn, trip).await;
     }
+    sqlx::query("COMMIT").execute(&mut conn).await.unwrap();
 
     println!("done!");
 }
 
+async fn insert_trip(conn: &mut sqlx::pool::PoolConnection<sqlx::Sqlite>, trip: Trip) {
+    
+    sqlx::query("INSERT INTO trips (trip_id, direction_id, block_id, shape_id, route_id, service_id) VALUES (?, ?, ?, ?, ?, ?)")
+        .bind(trip.trip_id)
+        .bind(trip.direction_id)
+        .bind(trip.block_id)
+        .bind(trip.shape_id)
+        .bind(trip.route_id)
+        .bind(trip.service_id)
+        .execute(conn).await.ok();
+}
+
 async fn gtfs_zip_file(url: &str) -> Result<std::fs::File, std::io::Error> {
     let mut file = tempfile::tempfile()?;
-    println!("getting: {}", url);
-    //let mut res = awc::Client::default().get(url).send().await;
-    let mut res = reqwest::get(url).await.unwrap();
-    let mut bytes = res.bytes().await.unwrap();
+    println!("getting: {}\n", url);
+    let res = reqwest::get(url).await.unwrap();
+    let bytes = res.bytes().await.unwrap();
     std::io::copy(&mut bytes.as_ref(), &mut file).unwrap();
+    println!("got it.");
     Ok(file)
 }
